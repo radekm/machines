370d837aedf43d9571039a7ea7bf94891cabf778
Radek Micek <radek.micek@gmail.com>
2020-03-30 22:36:05 +0200

    Project skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.70"
+
+    // Apply the java-library plugin for API and implementation separation.
+    `java-library`
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
---
+++ src/main/kotlin/cz/radekm/machines/Machines.kt
@@ -0,0 +1,2 @@
+package cz.radekm.machines
+
