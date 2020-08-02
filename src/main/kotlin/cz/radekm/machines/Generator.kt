879d63190d41368944e68b0220c626ca3623d975
Radek Micek <radek.micek@gmail.com>
2020-08-02 13:44:01 +0200

    Add more generator functions

--- src/main/kotlin/cz/radekm/machines/Generator.kt
+++ src/main/kotlin/cz/radekm/machines/Generator.kt
@@ -175,6 +175,17 @@ fun <A> generatorOf(vararg elements: A): Generator<A> = if (elements.isEmpty()) 
     yieldAll(elements.iterator())
 }
 
+fun <T : Any> generateGenerator(seed: T?, nextFunction: (T) -> T?): Generator<T> =
+        if (seed == null) emptyGenerator()
+        else generator {
+            var s = seed
+            while (s != null) {
+                // Hmm, compiler warns that `!!` is unnecessary but when I remove it code doesn't compile :-(
+                yield(s!!)
+                s = nextFunction(s)
+            }
+        }
+
 fun <A> Generator<A>.take(howMany: Int): Generator<A> = transform {
     repeat(howMany) {
         val a = await()
@@ -186,6 +197,22 @@ fun <A> Generator<A>.take(howMany: Int): Generator<A> = transform {
     }
 }
 
+fun <A> Generator<A>.drop(howMany: Int): Generator<A> = transform {
+    repeat(howMany) {
+        val a = await()
+        if (a.isEmpty) {
+            return@repeat
+        }
+    }
+    while (true) {
+        val a = await()
+        if (a.isEmpty) {
+            break
+        }
+        yield(a.get())
+    }
+}
+
 fun <A, B> Generator<A>.flatMap(f: (A) -> Generator<B>): Generator<B> = transformItem { a ->
     val generatorB = f(a)
     generatorB.withMachine { machineB ->
@@ -194,6 +221,32 @@ fun <A, B> Generator<A>.flatMap(f: (A) -> Generator<B>): Generator<B> = transfor
 }
 
 fun <A, B> Generator<A>.map(f: (A) -> B): Generator<B> = transformItem { yield(f(it)) }
+
+fun <A> Generator<A>.filter(pred: (A) -> Boolean): Generator<A> = transformItem {
+    if (pred(it)) {
+        yield(it)
+    }
+}
+
+fun <A> Generator<A>.filterNot(pred: (A) -> Boolean): Generator<A> = filter { !pred(it) }
+
+fun <T : Any> Generator<T?>.filterNotNull(): Generator<T> {
+    @Suppress("UNCHECKED_CAST")
+    return filterNot { it == null } as Generator<T>
+}
+
+fun <A> Generator<A>.filterIndexed(pred: (Int, A) -> Boolean): Generator<A> = transform {
+    var index = 0
+    while (true) {
+        val a = await()
+        if (a.isEmpty) {
+            break
+        }
+        if (pred(index, a.get()))
+            yield(a.get())
+        index++
+    }
+}
 
 // TODO Should be check that both generators generate same number of elements?
 infix fun <A, B> Generator<A>.zip(other: Generator<B>): Generator<Pair<A, B>> = generator {
@@ -212,11 +265,24 @@ infix fun <A, B> Generator<A>.zip(other: Generator<B>): Generator<Pair<A, B>> = 
     }
 }
 
-fun <A> Generator<A>.forEach(f: (A) -> Unit) = transformItem<Nothing> { f(it) }.run()
+// This doesn't support inlining.
+//fun <A> Generator<A>.forEach(f: (A) -> Unit) = transformItem<Nothing> { f(it) }.run()
+
+inline fun <A> Generator<A>.forEach(f: (A) -> Unit) {
+    withMachine { machine ->
+        machine.forEachAwaited(f)
+    }
+}
 
 fun <A> Generator<A>.toList(): List<A> {
     val result = mutableListOf<A>()
     forEach { result += it }
+    return result
+}
+
+fun <A> Generator<A>.count(): Int {
+    var result = 0
+    forEach { result++ }
     return result
 }
 
